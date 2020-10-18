ea0876bb8ac2b94d77432a1715fcc7e1e5ce5ce2
Rafael Garcia <rafagrcb@gmail.com>
2020-10-18 15:30:58 +0000

    Done.

---
+++ paramable.rb
@@ -0,0 +1,34 @@
+module Paramable
+  module InstanceMethods
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+  end
+end
+
+module Findable
+  module ClassMethods
+    def find_by_name(name)
+      self.all.detect{|o| o.name}
+    end    
+  end
+end
+
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
+end
