5da524a78110901bd5b3691d713558317674bc86
Rafael Garcia <rafagrcb@gmail.com>
2020-10-18 15:29:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,11 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
+  
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@songs = []
 
