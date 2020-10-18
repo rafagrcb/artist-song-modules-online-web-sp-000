a93467485f38f9a79e15fd03ad5864437ab8b8e3
Rafael Garcia <rafagrcb@gmail.com>
2020-10-18 15:30:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,6 @@ class Song
   attr_accessor :name
   attr_reader :artist
   
-  extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
